387e34fe34e5dd3f72d6fd3d5308a1292526e74d
Martin Zeman <martin.zeman@me.com>
2015-07-09 04:41:45 +0200

    Added module for composure

---
+++ modules/composure/init.zsh
@@ -0,0 +1,13 @@
+##
+# @file
+#     composure/init.zsh
+#
+# @brief
+#     These light-hearted functions make programming the shell easier and more intuitive
+#
+# @author
+#     Martin Zeman <https://github.com/N4M3Z>
+#
+
+## Source module files.
+source "${0:h}/external/composure.sh" || return 1
