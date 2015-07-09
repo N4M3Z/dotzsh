a86e0e618c748df93c07561e8bc02541f3bb8c15
Martin Zeman <martin.zeman@me.com>
2015-07-09 04:32:33 +0200

    Added module for alias-tips

---
+++ modules/alias-tips/init.zsh
@@ -0,0 +1,17 @@
+##
+#  @file
+#       alias-tips/init.zsh
+#
+#  @brief
+#       Integrates djui/alias-tips.
+#
+#  @author
+#       Martin Zeman <martin.zeman@protonmail.ch>
+#
+
+# Set default text.
+export ZSH_PLUGINS_ALIAS_TIPS_TEXT="Alias tip: " # Variable needs to be exported first
+zstyle -s ":prezto:module:alias-tips" text "ZSH_PLUGINS_ALIAS_TIPS_TEXT"
+
+# Source module files.
+source "${0:h}/external/alias-tips.plugin.zsh" || return 1
