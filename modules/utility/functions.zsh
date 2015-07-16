519c766142b2f777aed6895b74976b2665cddcf4
Martin Zeman <martin.zeman@me.com>
2015-07-16 06:02:28 +0200

    Updated to modular structure

---
+++ modules/utility/functions.zsh
@@ -0,0 +1,48 @@
+##
+#  @file
+#      utility/functions.zsh
+#
+#  @brief
+#       Functions for the 'utility' module
+#
+#  @author
+#       Robby Russell <robby@planetargon.com>
+#       Suraj N. Kurapati <sunaku@gmail.com>
+#       Sorin Ionescu <sorin.ionescu@gmail.com>
+#       Martin Zeman <martin.zeman@protonmail.ch>
+#
+
+# Makes a directory and changes to it.
+function mkdcd {
+  [[ -n "$1" ]] && mkdir -p "$1" && builtin cd "$1"
+}
+
+# Changes to a directory and lists its contents.
+function cdls {
+  builtin cd "$argv[-1]" && ls "${(@)argv[1,-2]}"
+}
+
+# Pushes an entry onto the directory stack and lists its contents.
+function pushdls {
+  builtin pushd "$argv[-1]" && ls "${(@)argv[1,-2]}"
+}
+
+# Pops an entry off the directory stack and lists its contents.
+function popdls {
+  builtin popd "$argv[-1]" && ls "${(@)argv[1,-2]}"
+}
+
+# Prints columns 1 2 3 ... n.
+function slit {
+  awk "{ print ${(j:,:):-\$${^@}} }"
+}
+
+# Finds files and executes a command on them.
+function find-exec {
+  find . -type f -iname "*${1:-}*" -exec "${2:-file}" '{}' \;
+}
+
+# Displays user owned processes status.
+function psu {
+  ps -U "${1:-$LOGNAME}" -o 'pid,%cpu,%mem,command' "${(@)argv[2,-1]}"
+}
