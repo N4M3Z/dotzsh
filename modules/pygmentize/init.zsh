b902495a81f353ece4db694da1c7b9eed38c4689
Martin Zeman <martin.zeman@me.com>
2015-08-31 19:38:41 +0200

    Added comment header

--- modules/pygmentize/init.zsh
+++ modules/pygmentize/init.zsh
@@ -1,13 +1,23 @@
+##
+#  @file
+#
+#  @brief
+#      Integrates StackExchange/blackbox.
+#
+#  @author
+#      Martin Zeman <https://github.com/N4M3Z>
+#
+
 pmodload 'environment'
 
-# Syntax highlighting for cat
-if is-defined "pygmentize"; then
+# Determine if command exists
+if (( $+commands[pygmentize] ))
+then
     # Warning: Do not alias `cat` itself or you will be getting token errors (try with ^C)
     alias ccat="pygmentize -g"
     alias -g '§P'='2>&1| pygmentize -l'
     # if pygmentize fails for utf-8, use "-O encoding=UTF-8"
 else
-    echo "Pygmentize not found on your system, installing ..."
+    echo "Pygmentize not found on your system, please install it, e.g.:"
     echo "> pip install Pygments"
-    pip install Pygments
 fi
