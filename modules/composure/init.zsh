e67c90e6196f53b80338bd68d432baf62a04a3d9
Martin Zeman <martin.zeman@protonmail.com>
2017-07-27 03:17:45 +0200

    [composure] Add support for erichs/composure

---
+++ modules/composure/init.zsh
@@ -0,0 +1,14 @@
+##
+# @file
+#     composure/init.zsh
+#
+# @brief
+#     These light-hearted functions make programming the shell easier and more intuitive
+#
+# @author
+#     Martin Zeman <https://github.com/N4M3Z>
+#
+
+## Source module files.
+zplug "erichs/composure", \
+    use:composure.sh
